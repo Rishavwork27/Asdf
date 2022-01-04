6d73b266df50a6e3806ecc0fac6a52267d36fb95
Rishavwork27 <96471227+Rishavwork27@users.noreply.github.com>
2022-01-04 14:34:27 +0530

    Test

---
+++ src/ASDF.java
@@ -0,0 +1,11 @@
+public class ASDF {
+    public static void main(String[] args){
+        int a=10,b=20,c=30;
+        int t=a;
+        a=b;
+        b=t;
+        System.out.println(" After Swapping Two number "+a+" "+b);
+
+    }
+
+}
